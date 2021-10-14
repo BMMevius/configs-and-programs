0481fd0198fb49019124ac94345fae5960146130
Bastiaan Mevius <b.m.mevius@student.tue.nl>
2021-10-14 16:11:51 +0200

    Add konsole plasma dep

--- install-pacman.sh
+++ install-pacman.sh
@@ -61,7 +61,7 @@ echo "Install packages on new system..."
 pacstrap /mnt base linux linux-firmware linux-headers \
     iwd iw lutris steam sudo docker docker-compose nvidia cuda cuda-tools xf86-video-intel mesa zsh man aws-cli \
     openvpn git qtcreator qt6 base-devel dlang firefox networkmanager-openvpn grub efibootmgr nano vi vifm sddm \
-    wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls \
+    wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls code konsole \
     mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error \
     lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo \
     sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama \
