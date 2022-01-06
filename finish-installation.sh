f8654e9b833f21e0883b1a7dbd9d41891d3d453f
BMMevius <36536314+BMMevius@users.noreply.github.com>
2022-01-06 11:47:12 +0100

    Create finish-installation.sh

---
+++ finish-installation.sh
@@ -0,0 +1,5 @@
+!#/usr/bin/env bash
+
+git clone git@github.com:zdharma-continuum/history-search-multi-word.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/history-search-multi-word
+git clone git@github.com:zsh-users/zsh-autosuggestions.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
+git clone git@github.com:zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
