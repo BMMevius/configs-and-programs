7c36490bc09bc74fa44c6d386acce5362f3f6ea3
Bastiaan Mevius <bastiaanmevius@gmail.com>
2022-01-15 16:10:14 +0100

    Fix shebang

--- finish-installation.sh
+++ finish-installation.sh
@@ -1,4 +1,4 @@
-!#/usr/bin/env bash
+#!/usr/bin/env bash
 
 git clone git@github.com:zdharma-continuum/history-search-multi-word.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/history-search-multi-word
 git clone git@github.com:zsh-users/zsh-autosuggestions.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
