f6995ee723d2236c2f8ff0b4a904d0ab8272b3d0
Bastiaan Mevius <bastiaanmevius@gmail.com>
2022-12-19 20:33:30 +0000

    Use azure-storage-fuse aur package instead of compile from scratch. Close #42

--- finish-installation.sh
+++ finish-installation.sh
@@ -1,31 +1,6 @@
 #!/usr/bin/env bash
 
-set -e
-
 # Install ZSH plugins
 git clone git@github.com:zdharma-continuum/history-search-multi-word.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/history-search-multi-word
 git clone git@github.com:zsh-users/zsh-autosuggestions.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
 git clone git@github.com:zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
-
-# Install blobfuse2
-# https://learn.microsoft.com/en-us/azure/storage/blobs/blobfuse2-how-to-deploy#install-blobfuse2
-cd ~
-git clone https://github.com/Azure/azure-storage-fuse/
-cd ./azure-storage-fuse
-git checkout main
-
-# Build
-yay -Sy go
-go get
-./build
-sudo mv blobfuse2 /usr/local/bin/blobfuse2
-
-# Clean up
-cd ~
-rm -rf azure-storage-fuse
-yay -R go
-
-# Mount azure blob filesystem
-mount_path=/run/media/azure
-sudo mkdir "$mount_path"
-sudo systemctl start blobfuse2
