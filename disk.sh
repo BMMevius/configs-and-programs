ed3f7456ab86a73ef671a951bb3c8c2ff8e7ec74
Bastiaan Mevius <bastiaanmevius@gmail.com>
2022-12-29 13:26:26 +0000

    Add automatic disk partitioning

--- disk.sh
+++ disk.sh
@@ -1,19 +1,36 @@
 #!/usr/bin/env bash
 
 read -d "\n" -r -a partitions <<< "$(lsblk -l | awk '{if ($6=="part") print $1}')"
+read -d "\n" -r -a disks <<< "$(lsblk -l | awk '{if ($6=="disk") print $1}')"
+
+select_list() {
+    read -r -a items <<< "$1"
+    title=$2
+
+    PS3=$title
+    select item in "${items[@]}"
+    do
+        if [ -n "$item" ]; then
+            break
+        fi
+    done
+    export item
+}
 
 select_partition() {
     title=$1
 
-    PS3=$title
-    select partition in "${partitions[@]}"
-    do
-        if [ -n "$partition" ]; then
-            break
-        fi
-    done
+    select_list "${partitions[*]}" "$title"
     echo "Selected partition: $partition"
-    export partition
+    export partition="/dev/$item"
+}
+
+select_disk() {
+    title=$1
+
+    select_list "${disks[*]}" "$title"
+    echo "Selected disk: $disk"
+    export disk="/dev/$item"
 }
 
 wipe_partition() {
@@ -27,11 +44,11 @@ wipe_partition() {
     PS3="Wipe partition? (This will destroy all data contained on that partition!) "
     select answer in "yes" "no"
     do
-        umount "/dev/$partition"
+        umount "$partition"
         case $answer in
             yes)
                 echo "Wiping off data from partition $partition..."
-                mkfs -t "$type" "${fat_args[@]}" "/dev/$partition"
+                mkfs -t "$type" "${fat_args[@]}" "$partition"
                 break
                 ;;
             no)
@@ -42,14 +59,43 @@ wipe_partition() {
     done
 }
 
+automatic_disk_partitioning() {
+    PS3="Do you want to do automatic disk partitioning?
+This will create a boot partition and a partition for the OS.
+(This will destroy all data contained on that disk!) "
+    select answer in "yes" "no"
+    do
+        case $answer in
+            yes)
+                select_disk "Disk to automatically partition: "
+                echo "Repartitioning disk $disk..."
+                umount "$disk"
+                sfdisk --delete "$disk"
+                sfdisk "$disk" <<EOF
+0,2048
+;
+EOF
+                sfdisk --change-id "$disk" 1 C12A7328-F81F-11D2-BA4B-00A0C93EC93B
+                break
+                ;;
+            no)
+                echo "Skipping automatically partition"
+                break
+                ;;
+        esac
+    done
+}
+
+automatic_disk_partitioning
+
 export mount_path=/mnt
 umount "/mnt/boot"
 select_partition "Partition to install arch linux on: "
 wipe_partition "$partition" ext4
-e2label "/dev/$partition" "arch_os"
-mount --mkdir "/dev/$partition" "$mount_path"
+e2label "$partition" "arch_os"
+mount --mkdir "$partition" "$mount_path"
 
 select_partition "Boot partition: "
 wipe_partition "$partition" fat
-fatlabel "/dev/$partition" "boot"
-mount --mkdir "/dev/$partition" "/mnt/boot"
+fatlabel "$partition" "boot"
+mount --mkdir "$partition" "/mnt/boot"
