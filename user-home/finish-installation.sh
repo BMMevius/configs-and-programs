8c480264694f4354ada345971f36ca14f865ba99
Bastiaan Mevius <bastiaanmevius@gmail.com>
2024-02-25 08:00:20 +0000

    Add `ttf-mulish`. Close #68. And add satoshi as well.

--- user-home/finish-installation.sh
+++ user-home/finish-installation.sh
@@ -5,3 +5,22 @@ git clone git@github.com:zdharma-continuum/history-search-multi-word.git "${ZSH_
 git clone git@github.com:zsh-users/zsh-autosuggestions.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
 git clone git@github.com:zsh-users/zsh-syntax-highlighting.git "${ZSH_CUTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
 git clone https://github.com/bilelmoussaoui/flatpak-zsh-completion "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/flatpak
+
+# Install fonts
+font_dir=/usr/local/share/fonts
+mkdir -p "$font_dir"
+
+# Install satoshi font
+curl -fsSLO https://api.fontshare.com/v2/fonts/download/satoshi
+unzip satoshi -d satoshi_fonts
+mv satoshi_fonts/* "$font_dir"
+rm -rf satoshi_fonts satoshi
+
+# Install mulish font
+git clone git@github.com:googlefonts/mulish.git
+mkdir -p "$font_dir/mulish"
+cp mulish/fonts/ttf "$font_dir/mulish"
+rm -rf mulish
+
+# Update font cache
+fc-cache
